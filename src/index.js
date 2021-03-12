681eaa4d9506e95f7855863b6d44dd66e5b55742
ThiNguyen <dragon_thi46@yahoo.com>
2021-03-12 20:13:07 +0700

    add effect header

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 const headerButtonMenu = document.querySelector(".header__button--menu");
 const menuContent = document.querySelector(".header__menu");
+const header = document.querySelector("header");
 
 // Open menu Mobile
 headerButtonMenu.addEventListener("click", () => {
   menuContent.classList.toggle("header__menu--open");
+});
+
+// Scroll Header
+var lastScroll = 0;
+
+jQuery(document).ready(function ($) {
+  $(window).scroll(function () {
+    setTimeout(function () {
+      var scroll = $(window).scrollTop();
+      if (scroll > lastScroll + 10) {
+        $("#header").css({ top: "-80px" });
+      } else if (scroll < lastScroll - 10) {
+        $("#header").css({ top: "0" });
+      }
+      lastScroll = scroll;
+    });
+  });
 });
