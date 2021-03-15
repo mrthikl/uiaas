b0f060cf637e47f1a1c1c3f4717a92114ef79e5f
ThiNguyen <dragon_thi46@yahoo.com>
2021-03-15 09:47:11 +0700

    add scroll into section

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 const headerButtonMenu = document.querySelector(".header__button--menu");
 const menuContent = document.querySelector(".header__menu");
 const header = document.querySelector("header");
-
-// Open menu Mobile
-headerButtonMenu.addEventListener("click", () => {
-  menuContent.classList.toggle("header__menu--open");
-});
+const projectButton = document.getElementById("projectButton");
+const pricingButton = document.getElementById("pricingButton");
+const FAQButton = document.getElementById("FAQButton");
+const featuresButton = document.getElementById("featuresButton");
 
 // Scroll Header
 var lastScroll = 0;
@@ -24,6 +23,11 @@ jQuery(document).ready(function ($) {
   });
 });
 
+// Open menu Mobile
+headerButtonMenu.addEventListener("click", () => {
+  menuContent.classList.toggle("header__menu--open");
+});
+
 // aos js
 AOS.init({
   duration: 1000,
@@ -31,3 +35,46 @@ AOS.init({
 });
 
 AOS.refresh();
+
+// Scroll in to
+$("#projectButton").click(function () {
+  $("html, body").animate(
+    {
+      scrollTop: $("#project").offset().top,
+    },
+    1000
+  );
+});
+$("#pricingButton").click(function () {
+  $("html, body").animate(
+    {
+      scrollTop: $("#pricing").offset().top,
+    },
+    1000
+  );
+});
+$("#FAQButton").click(function () {
+  $("html, body").animate(
+    {
+      scrollTop: $("#FAQ").offset().top,
+    },
+    1000
+  );
+});
+$("#featuresButton").click(function () {
+  $("html, body").animate(
+    {
+      scrollTop: $("#features").offset().top,
+    },
+    1000
+  );
+});
+
+pricingButton.addEventListener("click", removeOpenMenu);
+projectButton.addEventListener("click", removeOpenMenu);
+FAQButton.addEventListener("click", removeOpenMenu);
+featuresButton.addEventListener("click", removeOpenMenu);
+
+function removeOpenMenu() {
+  menuContent.classList.remove("header__menu--open");
+}
